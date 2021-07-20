83110196b27168621d4cc303f9a98d96c07465e5
PratikNichite <74714690+PratikNichite@users.noreply.github.com>
2021-07-20 11:52:18 +0530

    Update solve.js

--- js/solve.js
+++ js/solve.js
@@ -217,7 +217,7 @@ nextButton.addEventListener("click", () => {
         sessionStorage.setItem("creatorName", creatorName);
         sessionStorage.setItem("solverSet", answerSet);
         window.location.href = "../redirect/show-quiz-answers.html";
-      }, 300);
+      }, 500);
     } else {
       blackScreen.style.visibility = "visible";
       whitescreen.style.visibility = "visible";
