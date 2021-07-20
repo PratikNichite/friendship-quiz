11a46fccde59d07a679780d62d0eff411c6fc5ef
PratikNichite <74714690+PratikNichite@users.noreply.github.com>
2021-07-20 11:46:52 +0530

    Update solve.js

--- js/solve.js
+++ js/solve.js
@@ -210,12 +210,14 @@ nextButton.addEventListener("click", () => {
       pageTransition.classList.remove("page-transition");
       clearOptions();
       sendScore();
-      sessionStorage.clear();
-      sessionStorage.setItem("hostSet", JSON.stringify(solverQuestionSet));
-      sessionStorage.setItem("score", score);
-      sessionStorage.setItem("creatorName", creatorName);
-      sessionStorage.setItem("solverSet", answerSet);
-      window.location.href = "../redirect/show-quiz-answers.html";
+      SetTimeout(()=>{
+        sessionStorage.clear();
+        sessionStorage.setItem("hostSet", JSON.stringify(solverQuestionSet));
+        sessionStorage.setItem("score", score);
+        sessionStorage.setItem("creatorName", creatorName);
+        sessionStorage.setItem("solverSet", answerSet);
+        window.location.href = "../redirect/show-quiz-answers.html";
+      }, 300);
     } else {
       blackScreen.style.visibility = "visible";
       whitescreen.style.visibility = "visible";
