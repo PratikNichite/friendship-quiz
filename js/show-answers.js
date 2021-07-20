23b29553ca0e73061cb891a39567b087f69d50e6
PratikNichite <74714690+PratikNichite@users.noreply.github.com>
2021-07-20 11:40:48 +0530

    Update show-answers.js

--- js/show-answers.js
+++ js/show-answers.js
@@ -31,6 +31,15 @@ const showScore = () => {
   scoreBoard.appendChild(h2);
 };
 
+const QuestionModifier = (i, question) => {
+  var newQuestion =
+    "Q." + (i + 1) + " " + question;
+  newQuestion = newQuestion.replace("do you", "does " + creatorName);
+  newQuestion = newQuestion.replace("your", creatorName + "'s");
+  newQuestion = newQuestion.replace("you", creatorName);
+  return newQuestion;
+};
+
 const showAnswers = () => {
   answerTitle.innerHTML =
     "Answers of " + creatorName + "'s quiz are as follows!";
@@ -39,7 +48,7 @@ const showAnswers = () => {
   answerTitle.appendChild(help);
   for (var i = 0; i < hostSet.length; i++) {
     const p = document.createElement("p");
-    p.innerHTML = `Q.${i + 1} ${hostSet[i].question}`;
+    p.innerHTML = `Q.${i + 1} ${QuestionModifier(i,hostSet[i].question)}`;
     const questionDiv = document.createElement("div");
     questionDiv.appendChild(p);
     questionDiv.classList.add("answer-list__question");
